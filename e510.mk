39eb66d1d8aeac43893b8ea654bd2c8aaa4b0770
markolino631 <markolino-leggends@live.it>
2012-05-27 20:49:19 +0200

    Updates

--- e510.mk
+++ e510.mk
@@ -4,8 +4,6 @@ PRODUCT_AAPT_PREF_CONFIG := mdpi
 $(call inherit-product, device/lge/msm7x27-common/device.mk)
 $(call inherit-product-if-exists, vendor/lge/e510/e510-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/lge/e510/overlay
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qt602240_ts_input.kl:system/usr/keylayout/qt602240_ts_input.kl \
     $(LOCAL_PATH)/configs/univa_keypad.kl:system/usr/keylayout/univa_keypad.kl \
