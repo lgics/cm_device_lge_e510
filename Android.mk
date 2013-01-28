4ad9911b24edcdfeb2ec12b66680784eb5ec8411
Piso94 <pisoni.luca94@gmail.com>
2013-01-28 02:37:27 +0100

    Ops!

--- Android.mk
+++ Android.mk
@@ -1,5 +1,6 @@
 ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),e510)
 include $(call first-makefiles-under,$(call my-dir))
-$shell(mkdir -p $(OUT)/data)
-$shell($(call my-dir)/echo_nvmac.sh)
+#Made work Bluetooth Name & Paired
+$(shell mkdir -p $(OUT)/data)
+$(shell $(call my-dir)/echo_nvmac.sh)
 endif
